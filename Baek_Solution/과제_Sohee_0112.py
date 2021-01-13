5bb7bc407bbb14e5fe222d686119dfe9b52972af
Kwon Sohee <76766429+KwonSohee0620@users.noreply.github.com>
2021-01-13 21:15:22 +0900

    Update 과제_Sohee_0112.py

--- Baek_Solution/과제_Sohee_0112.py
+++ Baek_Solution/과제_Sohee_0112.py
@@ -1,3 +1,30 @@
-#과제
+#과제2 가장 큰 수 찾기 권소희 
 
-n = int(input())
+def solution(numbers):
+    answer = ''
+    f_answer = ''
+    numbers = list(map(str, numbers))
+
+    for i in numbers:
+        if len(i) == 4:
+            answer.append([i,i*3])
+        if len(i) == 3:
+            answer.append([i,i*4])
+        if len(i) == 2:
+            answer.append([i,i*6])
+        if len(i) == 1:
+            answer.append([i,i*12])
+
+    #numbers.sort(key = lambda x : x*3, reverse=True)
+    #이런 간단한...식이..있었군..
+    for i in numbers:
+        f_answer.append(i[0])
+
+    if len(numbers) == numbers.count('0'):
+        return '0'
+    else:
+        answer = "".join(f_answer)
+        return f_answer
+
+numbers = [int(x) for x in input().split()]
+print(solution(numbers))
